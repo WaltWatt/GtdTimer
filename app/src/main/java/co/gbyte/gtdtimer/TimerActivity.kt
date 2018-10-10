1783f2c9b5ecc84331726523d131fbf32fce0b52
Walt <walt@gbyte.co>
2018-10-10 13:44:44 +1100

    Use variabes for the versions of dependecies/libs

--- app/src/main/java/co/gbyte/gtdtimer/TimerActivity.kt
+++ app/src/main/java/co/gbyte/gtdtimer/TimerActivity.kt
@@ -2,7 +2,6 @@ package co.gbyte.gtdtimer
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
